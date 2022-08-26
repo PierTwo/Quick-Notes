781148d848640f30744632ee0d0eaf83e641187d
PierTwo <105090994+PierTwo@users.noreply.github.com>
2022-08-26 07:29:23 -0500

    Added route for notes page

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+// Imports required modules
 const express = require("express");
 const path = require("path");
 
+// Sets PORT to bind to the environment port variable used by the Heroku server or to a local server port 0f 3001
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -16,6 +18,12 @@ app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
+// Get Route for notes page
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+
+// Tells the app to listen to any connections on the port set by the PORT variable
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
