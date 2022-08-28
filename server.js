72d5b06bbe029bd4997ef5274bfe513cd5468f1e
PierTwo <105090994+PierTwo@users.noreply.github.com>
2022-08-28 07:38:28 -0500

    Added wildcard route to the index page, added uuid dependency for unique IDs

--- server.js
+++ server.js
@@ -28,6 +28,10 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
+app.get("/*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
 // Tells the app to listen to any connections on the port set by the PORT variable
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
